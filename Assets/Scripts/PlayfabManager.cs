6133dff58b2084e12e387d713f9a1c1846a20c03
wook-min <gaemicgr@gmail.com>
2025-11-13 21:44:34 +0900

    (2025.11.13.) 플레이팹 설치
    Playfab 설치 및 회원가입 패널 제작(에셋 참고)

---
+++ Assets/Scripts/PlayfabManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using PlayFab;
+using UnityEngine.UI;
+using Photon.Realtime;
+using PlayFab.ClientModels;
+using System.Collections;
+
+public class PlayfabManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private InputField addressInputField;
+    [SerializeField] private InputField passwardInputField;
+
+    public void Success(LoginResult loginResult)
+    {
+        PhotonNetwork.AutomaticallySyncScene = false;
+
+        PhotonNetwork.GameVersion = "1.0f";
+
+        StartCoroutine(Connect());
+    }
+
+    private IEnumerator Connect()
+    {
+        // Name server에서 Master Server로 넘어가는 중...
+        PhotonNetwork.ConnectUsingSettings(); // 마스터 서버로 접속하는 함수
+
+        // 서버 연결이 완료되거나 시간 초과될 때까지 대기
+        while (PhotonNetwork.IsConnectedAndReady == false)
+        {
+            yield return null;
+        }
+
+        // 특정 로비를 생성하여 진입하는 함수
+        PhotonNetwork.JoinLobby();
+    }
+}
