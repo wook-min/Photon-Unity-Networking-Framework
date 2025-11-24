91c113107bd637a2cc9cdc4d6f1d79bb2ad5a894
wook-min <gaemicgr@gmail.com>
2025-11-24 21:44:10 +0900

    (2025.11.24.) 네트워크 객체
    유니티3 강의9
    멀티 접속용 네트워크 객체 생성
    플레이어 생성 및 카메라를 통한 1인칭
    +
    Photon 멤버함수를 통해 시점 개별화

---
+++ Assets/Scripts/CreateManager.cs
@@ -0,0 +1,14 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class CreateManager : MonoBehaviourPunCallbacks
+{
+    [Header("생성 위치 및 회전")]
+    [SerializeField] private Vector3 position;
+    [SerializeField] private Quaternion rotation;
+
+    private void Start()
+    {
+        PhotonNetwork.Instantiate("Character", position, rotation);
+    }
+}
