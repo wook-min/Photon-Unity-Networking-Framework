c45529ae54071acd7da2d5687840ae642f70de76
wook-min <gaemicgr@gmail.com>
2025-11-26 21:43:37 +0900

    (2025.11.26.) 동기화 시스템
    유니티3 강의11
    photonView.IsMine 변수를 통해 플레이어가 나 자신일때만 카메라를 온오프하게 하기.
    +
    CharacterController 컴포넌트를 통해 캐릭터 움직임 제어하기(기존의 리지드바디, Transform, Vector와 또 다른 방식)
    강체가 아니기 때문에 Update에서 일괄 처리할 것.

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,60 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class Character : MonoBehaviourPun
+{
+    [Header("카메라 관련")]
+    [SerializeField] private Camera remoteCamera;
+    [SerializeField] private CharacterController controller;
+
+    [Header("캐릭터 움직임 관련")]
+    [SerializeField] private float speed = 3f;
+    [SerializeField] Vector3 direction;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Start()
+    {
+        DisableCamera();
+    }
+
+    private void Update()
+    {
+        if (photonView.IsMine)
+        {
+            Control();
+            Move();
+        }
+    }
+
+
+    public void Control()
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.z = Input.GetAxisRaw("Vertical");
+
+        // direction 방향을 단위 벡터로 설정합니다.
+        direction.Normalize();
+    }
+
+    public void Move()
+    {
+        controller.Move(direction * speed * Time.deltaTime);
+    }
+
+    public void DisableCamera()
+    {
+        // 현재 플레이어가 나 자신이라면
+        if (photonView.IsMine)
+        {
+            Camera.main.gameObject.SetActive(false);
+        }
+        else // 원격 객체라면
+        {
+            remoteCamera.gameObject.SetActive(false);
+        }
+    }
+}
